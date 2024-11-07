3f14ec0693a7c4ab60b893d4d3d6d7804601baf7
Zubair Ayub <zubairayub@hotmail.com>
2024-11-07 22:59:22 +0500

    login function and design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,74 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+    <section class="login-content">
+               <div class="row m-0 align-items-center bg-white vh-100">
+                     <div class="col-md-6 d-md-block d-none bg-primary p-0 vh-100 overflow-hidden">
+                     <img src="https://images.pexels.com/photos/6863336/pexels-photo-6863336.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="img-fluid gradient-main animated-scaleX" alt="images" loading="lazy">
+                  </div>
+                  <div class="col-md-6">
+                     <div class="row justify-content-center">
+                        <div class="col-md-10">
+                           <div class="card card-transparent auth-card shadow-none d-flex justify-content-center mb-0">
+                              <div class="card-body">
+                                 <a href="#" class="navbar-brand d-flex align-items-center mb-3 justify-content-center">
+                                    <!--Logo start-->
+                                    <div class="logo-main">
+                                        <div class="logo-normal mb-2">
+                                            <img src="https://promptfilings.com/wp-content/uploads/2024/03/logo-h-1.png" alt="Logo" width="170">
+                                        </div>
+                                        <div class="logo-mini">
+                                            <img src="https://promptfilings.com/wp-content/uploads/2024/03/logo-h-1.png" alt="Logo" width="200">
+                                        </div>
+                                    </div>
+                                    <!--logo End-->
+                                     
+                                 </a>
+                                 <h3 class="mb-4 text-center">Sign In</h3>
+                                 <p class="text-center">Let Prompt Filings streamline the process.</p>
+                                 <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="row">
+                                       <div class="col-lg-12">
+                                          <div class="form-group">
+                                             <label for="email" class="form-label">Email</label>
+                                             <input type="email" class="form-control" id="email" aria-describedby="email" name="email" :value="old('email')" required autofocus autocomplete="username">
+                                             <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                            </div>
+                                       </div>
+                                       <div class="col-lg-12">
+                                          <div class="form-group">
+                                             <label for="password" class="form-label">Password</label>
+                                             <input type="password" class="form-control" id="password" aria-describedby="password"  name="password"
+                                             required autocomplete="current-password">
+                                             <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                            </div>
+                                       </div>
+                                       <div class="col-lg-12 d-flex justify-content-between">
+                                          <div class="form-check mb-3">
+                                             <input type="checkbox" class="form-check-input" id="customCheck1">
+                                             <label class="form-check-label" for="customCheck1">Remember Me</label>
+                                          </div>
+                                          <a href="{{ route('password.request') }}">Forgot Password?</a>
+                                       </div>
+                                    </div>
+                                    <div class="d-flex justify-content-center">
+                                       <button type="submit" class="btn btn-primary">Sign In</button>
+                                    </div>
+                                   
+                                    <p class="mt-3 text-center">
+                                       Don’t have an account? <a href="{{ route('register') }}" class="text-underline">Click here to sign up.</a>
+                                    </p>
+                                 </form>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="sign-bg sign-bg-right">
+                         <img src="https://promptfilings.com/wp-content/uploads/2024/04/symbol-300x300.png" style="position: fixed; right:-100px;opacity: .1;transform: rotate(-45deg);" width="300" alt="">
+                     </div>
+                  </div>
+               </div>
+            </section>
+    
 </x-guest-layout>
