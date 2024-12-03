af36c069289edb246544f605a44946640098b41d
Zubair Ayub <zubairayub@hotmail.com>
2024-12-03 14:40:59 +0500

    Update PaymentController.php

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -19,36 +19,43 @@ class PaymentController extends Controller
     public function createTransaction(Request $request)
     {
         $apiLoginId = config('authorize.api_login_id');
-        $transactionKey = config('authorize.transaction_key');
+$transactionKey = config('authorize.transaction_key');
 
-        $creditCard = new CreditCardType();
-        $creditCard->setCardNumber($request->input('card_number'));
-        $creditCard->setExpirationDate($request->input('expiry_date'));
-        $creditCard->setCardCode($request->input('card_code'));
+// Prepare Credit Card Information
+$creditCard = new CreditCardType();
+$creditCard->setCardNumber($request->input('card_number'));
+$creditCard->setExpirationDate($request->input('expiry_date'));
+$creditCard->setCardCode($request->input('card_code'));
 
-        $payment = new PaymentType();
-        $payment->setCreditCard($creditCard);
+// Create Payment Object
+$payment = new PaymentType();
+$payment->setCreditCard($creditCard);
 
-        $order = new OrderType();
-        $order->setInvoiceNumber(rand(1, 1000));
-        $order->setDescription('Sample Payment Transaction');
+// Create Order Object
+$order = new OrderType();
+$order->setInvoiceNumber(rand(1, 1000));
+$order->setDescription('Sample Payment Transaction');
 
-        $transactionRequest = new TransactionRequestType();
-        $transactionRequest->setTransactionType('authCaptureTransaction');
-        $transactionRequest->setAmount($request->input('amount'));
-        $transactionRequest->setPayment($payment);
-        $transactionRequest->setOrder($order);
+// Create Transaction Request
+$transactionRequest = new TransactionRequestType();
+$transactionRequest->setTransactionType('authCaptureTransaction');
+$transactionRequest->setAmount($request->input('amount'));
+$transactionRequest->setPayment($payment);
+$transactionRequest->setOrder($order);
 
-        $merchantAuthentication = new MerchantAuthenticationType();
-        $merchantAuthentication->setName($apiLoginId);
-        $merchantAuthentication->setTransactionKey($transactionKey);
+// Merchant Authentication
+$merchantAuthentication = new MerchantAuthenticationType();
+$merchantAuthentication->setName($apiLoginId);
+$merchantAuthentication->setTransactionKey($transactionKey);
 
-        $createTransactionRequest = new CreateTransactionRequest();
-        $createTransactionRequest->setMerchantAuthentication($merchantAuthentication);
-        $createTransactionRequest->setTransactionRequest($transactionRequest);
+// Create the Transaction Request
+$createTransactionRequest = new CreateTransactionRequest();
+$createTransactionRequest->setMerchantAuthentication($merchantAuthentication);
+$createTransactionRequest->setTransactionRequest($transactionRequest);
 
-        $controller = new CreateTransactionController($createTransactionRequest);
-        $response = $controller->executeWithApiResponse(ANetEnvironment::SANDBOX);
+// Execute the API Request
+$controller = new CreateTransactionController($createTransactionRequest);
+$response = $controller->executeWithApiResponse(ANetEnvironment::SANDBOX);
 
         if ($response && $response->getMessages()->getResultCode() === 'Ok') {
             $transactionResponse = $response->getTransactionResponse();
