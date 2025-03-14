fc2099ae36a0f478da0bec4904b1d8f5db9fbb78
Zubair Ayub <zubairayub@hotmail.com>
2025-03-14 20:40:45 +0500

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -114,8 +114,8 @@ class RegisteredUserController extends Controller
         // Send welcome email
         // Get admin email from .env file
         $adminEmail = env('MAIL_FROM_ADDRESS', 'noreply@promptfilings.com');
-        Mail::to($user->email)->send(new WelcomeMail($user));
-        Mail::to($adminEmail)->send(new WelcomeMail($user));
+        Mail::to('zubairayub@hotmail.com')->send(new WelcomeMail($user));
+        //Mail::to($adminEmail)->send(new WelcomeMail($user));
         Auth::login($user);
 
         return redirect(route('dashboard', absolute: false));
